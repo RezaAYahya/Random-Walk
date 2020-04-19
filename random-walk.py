4c2a05a634ab5d236ef1bcefe12ad13af2786145
rezaayahya <rezaahmadyahya@gmail.com>
2020-04-19 10:10:42 +0700

    Edit proses iteration and add plotting

--- random-walk.py
+++ random-walk.py
@@ -1,7 +1,7 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from random import randrange, random
+import numpy
+import pylab
+import random
+import random
 
 # Initialization
 n_particle = 10
@@ -10,16 +10,22 @@ x_pos = [0]
 y_pos = [0]
 
 # Iteration
-for i in range(n_iter):
-  for j in range(n_particle):
-    # Generate Random
-    rand = random.uniform(0,1)
-    # Update X and Y position based on the defined probability
-    if rand <= 0.25 :
-      x_pos[i+1] = x_pos[i] + 1
-    elif rand <= 0.5 :
-      y_pos[i+1] = y_pos[i] - 1
-    elif rand <= 0.75:
-      x_pos[i+1] = x_pos[i] - 1
-    else :
-      y_pos[i+1] = y_pos[i] + 1
+for i in range(1, n_iter):
+  rand = random.uniform(0, 1)
+  if rand <= 0.25:
+    x_pos.append(x_pos[i - 1] + 1)
+    y_pos.append(y_pos[i - 1])
+  elif rand <= 0.5:
+    x_pos.append(x_pos[i - 1])
+    y_pos.append(y_pos[i - 1] - 1)
+  elif rand <= 0.75:
+    x_pos.append(x_pos[i - 1] - 1)
+    y_pos.append(y_pos[i - 1])
+  else:
+    x_pos.append(x_pos[i - 1])
+    y_pos.append(y_pos[i - 1] + 1)
+
+# Plotting
+pylab.title("Random Walk 2D ($n = " + str(n_iter) + "$ steps)")
+pylab.plot(x_pos, y_pos, 'r')
+pylab.show()
