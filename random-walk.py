df809bd2f9113de30d2f9050befac3fcf43236fb
rezaayahya <rezaahmadyahya@gmail.com>
2020-04-20 22:00:55 +0700

    Edit plotting

--- random-walk.py
+++ random-walk.py
@@ -1,31 +1,32 @@
 import numpy
 import pylab
 import random
-import random
 
 # Initialization
 n_particle = 10
 n_iter = 100
-x_pos = [0]
-y_pos = [0]
+x_pos = numpy.zeros((n_particle, n_iter))
+y_pos = numpy.zeros((n_particle, n_iter))
 
 # Iteration
-for i in range(1, n_iter):
-  rand = random.uniform(0, 1)
-  if rand <= 0.25:
-    x_pos.append(x_pos[i - 1] + 1)
-    y_pos.append(y_pos[i - 1])
-  elif rand <= 0.5:
-    x_pos.append(x_pos[i - 1])
-    y_pos.append(y_pos[i - 1] - 1)
-  elif rand <= 0.75:
-    x_pos.append(x_pos[i - 1] - 1)
-    y_pos.append(y_pos[i - 1])
-  else:
-    x_pos.append(x_pos[i - 1])
-    y_pos.append(y_pos[i - 1] + 1)
+for j in range(n_particle):
+  for i in range(1, n_iter):
+    rand = random.uniform(0, 1)
+    if rand <= 0.25:
+      x_pos[j][i] = x_pos[j][i - 1] + 1
+      y_pos[j][i] = y_pos[j][i - 1]
+    elif rand <= 0.5:
+      x_pos[j][i] = x_pos[j][i - 1]
+      y_pos[j][i] = y_pos[j][i - 1] - 1
+    elif rand <= 0.75:
+      x_pos[j][i] = x_pos[j][i - 1] - 1
+      y_pos[j][i] = y_pos[j][i - 1]
+    else:
+      x_pos[j][i] = x_pos[j][i - 1]
+      y_pos[j][i] = y_pos[j][i - 1] + 1
+
+  pylab.plot(x_pos[j], y_pos[j])
 
 # Plotting
-pylab.title("Random Walk 2D ($n = " + str(n_iter) + "$ steps)")
-pylab.plot(x_pos, y_pos, 'r')
+pylab.title("Random Walk 2D (" + str(n_iter) + " steps and " + str(n_particle) + " Partikel)")
 pylab.show()
